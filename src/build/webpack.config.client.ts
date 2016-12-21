09ff2809d78c1ffed87fe25fa3e7662148217a8d
Daniel Fallon <Daniel@Fallon.io>
2016-12-21 02:41:05 -0600

    prelim build files

--- src/build/webpack.config.client.ts
+++ src/build/webpack.config.client.ts
@@ -1,3 +1,18 @@
-/**
- * Created by Daniel Fallon on 12/19/2016.
- */
+import webpack = require("webpack");
+import { outDir } from "./webpack.config.common";
+
+export = {
+    entry: {
+        client: "../client/index",
+        common: "../common/index"
+    },
+    output: {
+        path: outDir,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
---
+++ src/build/webpack.config.common.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Daniel Fallon on 12/20/2016.
+ */
+
+import * as path from "path";
+
+export let outDir = path.join(__dirname,'..','..','dist');
+export let clientOut = path.join(outDir,'client');
