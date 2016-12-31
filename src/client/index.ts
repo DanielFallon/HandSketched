7bed8fa2fcd602a884a3e3383e68e31e7754513d
Gingerman611 <Mfgingerman@gmail.com>
2016-12-31 00:39:44 -0600

    foobar

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,20 @@
+let canvasSpace = document.getElementById("canvas-space");
+let canvas  = document.createElement("canvas") as HTMLCanvasElement;
+canvasSpace.appendChild(canvas);
+function resizeCanvas() {
+    canvas.height = canvasSpace.clientHeight;
+    canvas.width = canvasSpace.clientWidth;
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+let ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(100,100);
+ctx.stroke();
+ctx.closePath();
+
 
